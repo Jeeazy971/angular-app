70a7a8a8dbf991f27afee4dd9b1502270264a713
Josué Bayidikila <josue-dev@outlook.fr>
2024-11-02 01:28:39 +0100

    update farma conf and install this package

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,13 @@
 module.exports = function (config) {
   config.set({
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
+    plugins: [
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-jasmine-html-reporter"),
+      require("karma-coverage-istanbul-reporter"),
+      require("@angular-devkit/build-angular/plugins/karma"),
+    ],
     browsers: ["ChromeHeadless"],
     customLaunchers: {
       ChromeHeadlessCI: {
@@ -7,5 +15,8 @@ module.exports = function (config) {
         flags: ["--no-sandbox", "--disable-gpu"],
       },
     },
+    reporters: ["progress", "kjhtml"],
+    singleRun: true,
+    restartOnFileChange: true,
   });
 };
